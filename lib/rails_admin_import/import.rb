505640c851ca1b7cea2a5c7d45166de747444b55
gongsuochen <gongsuochen@163.com>
2014-10-21 13:25:22 +0800

    revert save

--- lib/rails_admin_import/import.rb
+++ lib/rails_admin_import/import.rb
@@ -65,7 +65,7 @@ module RailsAdminImport
                         FileUtils.copy(params[:file].tempfile, "#{Rails.root}/log/import/#{Time.now.strftime("%Y-%m-%d-%H-%M-%S")}-import.csv")
                     end
 
-                    before_import_save  params[:file].tempfile
+                    object.before_import_save  params[:file].tempfile
 
 
                 end
